2a015e31da7a49cdbd30ab88956c0c96ecbe9dd7
Muhammad Awais Shah <m.awaisshah228@gmail.com>
2024-06-28 11:40:55 +0500

    fixing

--- components/RealTimeChart.tsx
+++ components/RealTimeChart.tsx
@@ -30,23 +30,22 @@ const pythPriceServiceConnection = new PriceServiceConnection(hermesUrl, {
 })
 
 
-const seriesCount = 530;
+const seriesCount = 60;
 
 const apexOptions: ApexOptions = {
   chart: {
     type: 'line',
-    // height: 350,
     zoom: {
       enabled: true,
-      type:'xy'
+      type: 'xy'
     },
     animations: {
       enabled: true,
       easing: 'linear',
-      // dynamicAnimation: {
-      //   enabled: true,
-      //   speed: 1000,
-      // },
+        dynamicAnimation: {
+        enabled: true,
+        speed: 1000,
+      },
     },
     toolbar: {
       show: true,
@@ -55,84 +54,51 @@ const apexOptions: ApexOptions = {
   annotations: {
     yaxis: [
       {
-        y: 60,
+        // y: 60,
         borderColor: '#00E396',
-        // label: {
-        //   borderColor: '#00E396',
-        //   style: {
-        //     color: '#fff',
-        //     background: '#00E396',
-        //   },
-        //   text: 'Y-axis annotation on 8800',
-        // },
       },
     ],
   },
   tooltip: {
     enabled: true,
-    // x:{
-    //   show: true
-    // },
-    onDatasetHover:{
+    onDatasetHover: {
       highlightDataSeries: true
     }
   },
-  // dataLabels: {
-  //   enabled: true,
-  //   distributed:true
-  // },
   stroke: {
     curve: 'smooth',
-    width: 1,
+    width: 2,
   },
-
   title: {
     text: 'Sol/USD Price',
     align: 'left',
   },
   xaxis: {
     type: 'datetime',
-    labels: {
-      // datetimeUTC: false,
-      // formatter: function (val) {
-      //   return new Date(val).toLocaleTimeString('en-US', {
-      //     minute: '2-digit',
-      //     second: '2-digit',
-      //   });
-      // },
-      // datetimeFormatter: {
-      //   minute: 'HH:mm',
-      // },
-    },
   },
   yaxis: {
-    // min: 100,
-    // max: 165,
     labels: {
       formatter: (val) => val.toFixed(2),
     },
     title: { text: 'Sol/USD ' },
-    // floating: true
-    // opposite: true,
   },
   legend: {
     horizontalAlign: 'left',
   },
+  fill:{
+    type: 'solid'
+  }
 };
 
 type TSeriesData = { x: number; y: number | null }[];
 type TSeries = { data: TSeriesData }[];
 
 
-
-
-
 export function RealTimeChart() {
   const [series, setSeries] = useState<TSeries>([]);
   const [options, setOptions] = useState<ApexOptions>(() => apexOptions);
+
   const addData = async (priceFeed: PriceFeed) => {
-
-
     try {
       const uncheckedPrice: Price = priceFeed.getPriceUnchecked();
 
@@ -143,7 +109,6 @@ export function RealTimeChart() {
         timestamp: new BN(uncheckedPrice.publishTime),
       })
 
-
       const localDate = new Date(priceOracle.timestamp.toString() * 1000); // Convert seconds to milliseconds
 
       const dataPt = {
@@ -151,20 +116,35 @@ export function RealTimeChart() {
         y: Number(priceOracle.toUiPrice(2)),
       };
 
-     
       setSeries((prevSeries) => {
         const newData = [...(prevSeries[0]?.data || []), dataPt];
-        // Ensure the series data array does not exceed 60 items
+        // Ensure the series data array does not exceed 120 items
         if (newData.length > seriesCount) {
           newData.shift(); // Remove the oldest item
         }
+
+        // Calculate min and max values
+        const prices: any = newData.map(d => d.y);
+        const minPrice = Math.min(...prices);
+        const maxPrice = Math.max(...prices);
+        const yAxisMin = minPrice - (minPrice * 0.01);
+        const yAxisMax = maxPrice + (maxPrice * 0.01);
+
+        setOptions((prevOptions) => ({
+          ...prevOptions,
+          yaxis: {
+            ...prevOptions.yaxis,
+            min: yAxisMin,
+            max: yAxisMax,
+          },
+        }));
+
         return [{ data: newData }];
       });
 
     } catch (error) {
       console.log(error)
     }
-
   };
 
   useEffect(() => {
@@ -174,12 +154,9 @@ export function RealTimeChart() {
   }, [])
 
 
-
   return <ReactApexChart options={options} series={series} type="line" />;
 }
 
 export default function RealTimeChartTest() {
-
-
   return <RealTimeChart />;
 }
